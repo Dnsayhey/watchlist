52d29ee00d635047653f72e1ba192a707d7fd45f
dnsayhey <yanlei921@live.com>
2022-12-18 15:42:28 +0800

    Ready for deploy.

--- watchlist/__init__.py
+++ watchlist/__init__.py
@@ -4,7 +4,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-
 app = Flask(__name__)
 
 if sys.platform.startswith('win'):
@@ -12,9 +11,9 @@ if sys.platform.startswith('win'):
 else:
     prefix = 'sqlite:////'
 
-app.config['SECRET_KEY'] = 'False'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'guess_me?^_^')
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + \
-    os.path.join(os.path.dirname(app.root_path), 'data.db')
+    os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
