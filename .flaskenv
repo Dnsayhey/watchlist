1ce725a44d271a6e30e83350e4aa15f4db4660ba
dnsayhey <yanlei921@live.com>
2022-12-18 13:28:37 +0800

    Add unit test with unittest. Organize application with package.

--- .flaskenv
+++ .flaskenv
@@ -1,4 +1,5 @@
 # .flaskenv
+FLASK_APP=watchlist
 
 # FLASK_ENV在2.3版本被弃用
 # FLASK_ENV=development
---
+++ watchlist/__init__.py
@@ -0,0 +1,39 @@
+import os
+import sys
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+
+if sys.platform.startswith('win'):
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
+
+app.config['SECRET_KEY'] = 'False'
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + \
+    os.path.join(os.path.dirname(app.root_path), 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    from watchlist.models import User
+    user = User.query.get(int(user_id))
+    return user
+
+
+@app.context_processor
+def inject_user():
+    from watchlist.models import User
+    user = User.query.first()
+    return dict(user=user)
+
+
+from watchlist import views, errors, commands
